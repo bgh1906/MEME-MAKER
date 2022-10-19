735dbb1607fafd9e61d4f4f4f5066309b00447b7
배지환 <bgh1906@naver.com>
2022-10-19 17:24:55 +0900

    노마드코더 바닐라 JS로 그림 앱 만들기 1.2 경로 설정 방법 및 단절 진행

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;//캔버스 크기설정 js
 canvas.height = 800;//캔버스 크기설정 js
 
-//사각형 추가
-ctx.fillRect(50, 50, 100, 200)
+// 사각형 추가
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+// 기존의 경로와 단절
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
