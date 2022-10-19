2d6f6be4eb772e1ff6588e6171d11282c5b49da2
배지환 <bgh1906@naver.com>
2022-10-19 16:58:23 +0900

    노마드코더 바닐라 JS로 그림 앱 만들기 1.1 캔버스 삽입 및 사각형 드로잉

---
+++ app.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;//캔버스 크기설정 js
+canvas.height = 800;//캔버스 크기설정 js
+
+//사각형 추가
+ctx.fillRect(50, 50, 100, 200)
