be61b8e1531cf872a797c49c78d3bc07d54b67f4
배지환 <bgh1906@naver.com>
2022-10-19 17:51:51 +0900

    노마드코더 바닐라 JS로 그림 앱 만들기 1.4 집그리기

--- app.js
+++ app.js
@@ -3,23 +3,33 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;//캔버스 크기설정 js
 canvas.height = 800;//캔버스 크기설정 js
 
-// 사각형 추가
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
+// // 사각형 추가
+// ctx.rect(50, 50, 100, 100);
+// ctx.rect(150, 150, 100, 100);
+// ctx.rect(250, 250, 100, 100);
+// ctx.fill();
 
-// 기존의 경로와 단절
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill();
+// // 기존의 경로와 단절
+// ctx.beginPath();
+// ctx.rect(350, 350, 100, 100);
+// ctx.rect(450, 450, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.fill();
 
-// 브러쉬 이동 및 선 그리기
-ctx.moveTo(550, 550);
-ctx.lineTo(650, 550);
-ctx.lineTo(650, 650);
-ctx.lineTo(550, 650);
-ctx.lineTo(550, 550);
+// // 브러쉬 이동 및 선 그리기
+// ctx.moveTo(550, 550);
+// ctx.lineTo(650, 550);
+// ctx.lineTo(650, 650);
+// ctx.lineTo(550, 650);
+// ctx.lineTo(550, 550);
+// ctx.fill();
+
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
