c1279e87e9873fd8d6a9fc3dde05167fdc7a5823
배지환 <bgh1906@naver.com>
2022-10-20 09:37:59 +0900

    노마드코더 바닐라 JS로 그림 앱 만들기 2.0 마우스 이벤트를 사용하여 선을 그리는 좌표 지정

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ canvas.height = 800;//캔버스 크기설정 js
 // ctx.fillStyle = "red";
 // ctx.fill();
 
+
 // // 브러쉬 이동 및 선 그리기
 // ctx.moveTo(550, 550);
 // ctx.lineTo(650, 550);
@@ -23,6 +24,7 @@ canvas.height = 800;//캔버스 크기설정 js
 // ctx.lineTo(550, 650);
 // ctx.lineTo(550, 550);
 // ctx.fill();
+
 
 // // 집 그리기
 // ctx.fillRect(200, 200, 50, 200);
@@ -35,19 +37,56 @@ canvas.height = 800;//캔버스 크기설정 js
 // ctx.lineTo(450, 200);
 // ctx.fill();
 
-// 사람 몸통
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
 
-// 원그리기 얼굴
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+// // 사람 몸통
+// ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+// ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+// ctx.fillRect(260 - 40, 200 - 30, 60, 200);
 
-// 경로 초기화
-ctx.beginPath();
-ctx.fillStyle = "white";
-// 눈 그리기
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+// // 원그리기 얼굴
+// ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+// ctx.fill();
+
+// // 경로 초기화
+// ctx.beginPath();
+// ctx.fillStyle = "white";
+// // 눈 그리기
+// ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+// ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
+// ctx.fill();s
+
+
+// 2.0
+// Painting Lines
+
+// 선 굵기 지정
+ctx.lineWidth = 2;
+
+// 색상 배열
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#18dcff",
+    "#7d5fff",
+  ];
+
+function onClick(event) { // onClick 이벤트 선언
+    console.log(event);// event에 대한 정보 콘솔출력
+    ctx.beginPath();
+    ctx.moveTo(0, 0);// 선 시작경로
+    const color = colors[Math.floor(Math.random() * colors.length)];// 랜덤으로 선 색상 선택
+    ctx.strokeStyle = color;// 현재 ctx 색상을 위의 랜덤값으로 변경
+    ctx.lineTo(event.offsetX, event.offsetY);// 현재 마우스좌표
+    ctx.stroke();// 선 그리기
+}
+
+// 마우스 클릭시 onClick 이벤트 발생
+// canvas.addEventListener("click", onClick);
+// 마우스 이동시 onClick 이벤트 발생
+canvas.addEventListener("mousemove", onClick);
+
